37c076e17ed97b0113b2c2ab0b943719ce4d8676
sk3l10x1ng <58649792+sk3l10x1ng@users.noreply.github.com>
2019-12-08 07:53:46 +0000

    Add files via upload

---
+++ gitClone.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+import subprocess
+import sys
+import os
+
+def git():
+    path = sys.argv[1]
+    os.chdir(str(sys.argv[2]))
+    if os.path.exists(path):
+        with open(path,'r',encoding='utf-8') as f:
+            for line in f:
+                l = line.strip()
+                if len(l.strip()) != 0:
+                    print("#####################################################\n")
+                    subprocess.run(['git', 'clone',l])
+                    print("\n")
+
+git()
